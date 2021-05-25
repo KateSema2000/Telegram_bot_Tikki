9c2181bab3c93172ae5c10fd9945ad9d7863a53e
KateSema2000 <kateryna.semenets@nure.ua>
2021-05-25 12:21:16 +0300

    First commit.

---
+++ bot_cofig.py
@@ -0,0 +1,411 @@
+from csv import reader
+
+BOT_CONFIG = {
+    'intents': {
+        '': {
+            'examples': [''],
+            'responses': ['']
+        },
+    },
+    'failure_phrases': ['Попробуйте написать по другому.', 'Что-то непонятно', 'Возможно я не знаю.']
+}
+
+
+########################## аккуматизированные
+def parsing_akkym():
+    filename = 'сиоп_аккуматизация.csv'
+    file = open(filename, "r")
+    data = file.read()
+    dataset = []
+    file_akkym = ''
+    for row in data.split(';";"\n'):
+        if len(row) > 1:
+            dataset.append([])
+            for text in row.split(';'):
+                dataset[len(dataset) - 1].append(text.replace('"', '').replace('\n', '\\n'))
+
+    # формирую всех аккуматизированных
+    file_akkym += "    {'intents': {\n"
+    for row in dataset:
+        # print("        '",row[0],"': {\n            'examples':",row[1:3],",\n            'responses': ['",row[3],"']\n        },", sep='')
+        file_akkym += "        '" + row[0] + "': {\n            'examples': ['" + row[1] + "', '" + row[
+            2] + "'],\n            'responses': ['" + row[3] + ' [Фото](' + row[4] + ')' + "']\n        },\n"
+    all = 'Все аккуматизированные персонажи: '
+    for row in dataset:
+        all += row[0] + ', '
+    all += 'продолжение следует.'
+    file_akkym += "         'Все аккуматизированные персонажи': {\n            'examples': ['все аккуматизированные персонажи','Список аккуматизированных','переисли аккуматизированных','список злодеев','все злодеи'],\n            'responses': ['" + all + "']\n        },\n"
+    file_akkym += "    },}"
+    # print(file_akkym)
+
+    f = open('file_akkym.txt', 'w')
+    f.write(file_akkym)
+    a = eval(file_akkym)
+
+
+########################## квами
+def parsing_kvami():
+    filename = 'сиоп_квами.csv'
+    file = open(filename, "r")
+    data = file.read()
+    dataset = []
+    rows = data.split(';";"\n')
+
+    # вопросы отделили
+    qs_all = rows[0]
+    qs = qs_all.split(';')
+    iqs = []
+    for q in qs:
+        if len(q) > 1:
+            iqs.append([])
+            for text in q.split('?'):
+                iqs[len(iqs) - 1].append(text)
+
+    # отделяем имя квами и ответы на вопросы
+    answers = rows[1:]
+    split_answers = []
+    for row in answers:
+        split_answers.append([])
+        for text in row.split(';'):
+            if text:
+                split_answers[len(split_answers) - 1].append(text.replace('\n', '\\n'))
+
+    # отделим квами
+    kvami = []
+    person = []
+    answers = []
+    links = []
+    for answer in split_answers:
+        if answer:
+            kvami.append(answer[0])
+            person.append(answer[1].split('/'))
+            answers.append(answer[2:-1])
+            links.append(answer[-1])
+
+    # print(answers)
+
+    # сделаем ответы на вопросы
+    q_link = ['расскажи про', 'как выглядит', 'какой характер у']
+    file_kvami = "    {'intents': {\n"
+    for k, answ, l, p in zip(kvami, answers, links, person):
+        for qs, an in zip(iqs, answ):
+            if an:
+                file_kvami += "        '" + qs[0] + " " + k + "': {\n            'examples': ["
+                for a in qs[:-1]:
+                    file_kvami += "'" + a + " " + k + "',"
+                """if p and qs[0] == 'расскажи про':
+                    for pers in p:
+                        file_kvami += "'квами " + pers.split(' ')[0] + "',"
+                        file_kvami += "'квами " + pers + "'," """
+                file_kvami += "'" + qs[
+                    len(qs) - 1] + ' ' + k + "'" + "],\n            'responses': ['" + an
+                if qs[0] in q_link:
+                    file_kvami += ' [Фото](' + l + ')'
+                file_kvami += "']\n        },\n"
+
+    file_kvami += "    },}"
+    # print(file_kvami)
+    f = open('file_kvami.txt', 'w')
+    f.write(file_kvami)
+    a = eval(file_kvami)
+
+
+########################## персы
+def parsing_persons():
+    filename = 'сиоп_персонажи.csv'
+    file = open(filename, "r")
+    data = file.read()
+    rows = data.split(';";"\n')
+
+    # вопросы отделили
+    qs_all = rows[0]
+    qs = qs_all.split(';')
+    iqs = []
+    for q in qs:
+        if len(q) > 1:
+            iqs.append([])
+            for text in q.split('? '):
+                iqs[len(iqs) - 1].append(text.replace('?', ''))
+    # print(iqs)
+
+    # отделяем имя персонажей и ответы на вопросы
+    answers = rows[1:]
+    split_answers = []
+    for row in answers:
+        if len(row) > 1:
+            split_answers.append([])
+            for text in row.split(';'):
+                split_answers[len(split_answers) - 1].append(text.replace('\n', '\\n'))
+
+    # for a in split_answers:
+    #    print(a)
+    # print(iqs)
+
+    # отделим персонажей
+    persons = []
+    links = []
+    answers = []
+    for answer in split_answers:
+        persons.append([answer[0].split(' ')[0], answer[0]])
+        links.append(answer[-1])
+        answers.append(answer[1:-1])
+    # print(answers)
+    q_link = ['расскажи про', 'как выглядит', 'характер']
+    file_persons = "    {'intents': {\n"
+    for person, answ, link in zip(persons, answers, links):
+        for qs, an in zip(iqs, answ):
+            if an:
+                file_persons += "        '" + qs[0] + " " + person[1] + "': {\n            'examples': ["
+                for p in person:
+                    for a in qs[:]:
+                        file_persons += "'" + a + " " + p + "',"
+                file_persons += "],\n            'responses': ['" + an
+                if qs[0] in q_link:
+                    file_persons += ' [Фото](' + link + ')'
+                file_persons += "']\n        },\n"
+
+    file_persons += "    },\n}"
+    # print(file_persons)
+    f = open('file_persons.txt', 'w')
+    f.write(file_persons)
+    a = eval(file_persons)
+
+
+########################## супергерои
+def parsing_superhero():
+    filename = 'сиоп_супергерои.csv'
+    file = open(filename, "r")
+    data = file.read()
+    rows = data.split(';";"\n')
+    # print(rows)
+
+    # вопросы отделили
+    qs_all = rows[0]
+    qs = qs_all.split(';')
+    iqs = []
+    for q in qs:
+        if len(q) > 1:
+            iqs.append([])
+            for text in q.split('? '):
+                iqs[len(iqs) - 1].append(text.replace('?', ''))
+    # print(iqs)
+
+    # отделяем имя персонажей и ответы на вопросы
+    answers = rows[1:]
+    split_answers = []
+    for row in answers:
+        if len(row) > 1:
+            split_answers.append([])
+            for text in row.split(';'):
+                if text:
+                    split_answers[len(split_answers) - 1].append(text.replace('\n', '\\n'))
+
+    # for a in split_answers:
+    #   print(a)
+    # print(iqs)
+
+    # отделим персонажей
+    persons = []
+    kvami = []
+    answers = []
+    links = []
+    for answer in split_answers:
+        if answer:
+            persons.append([answer[0].split('/'), answer[1]])
+            kvami.append(answer[2])
+            answers.append(answer[3:-1])
+            links.append(answer[-1])
+    # print(answers)
+    # print(persons)
+    q_link = ['расскажи про', 'как выглядит', 'какой характер у']
+    file_superpersons = "    {'intents': {\n"
+    for p, answ, l, k in zip(persons, answers, links, kvami):
+        for qs, an in zip(iqs, answ):
+            if an:
+                file_superpersons += "        '" + qs[0] + " " + p[0][0] + "': {\n            'examples': ["
+                for ps in p[0]:
+                    for a in qs:
+                        file_superpersons += "'" + a + " " + ps + "',"
+                    if qs[0] == 'квами':
+                        file_superpersons += "'" + qs[0] + " " + p[1] + "',"
+                file_superpersons += "],\n            'responses': ['" + an.replace('\n', '')
+                if qs[0] in q_link:
+                    file_superpersons += ' [Фото](' + l + ')'
+                file_superpersons += "']\n        },\n"
+    file_superpersons += "    },\n}"
+    # print(file_superpersons)
+    f = open('file_superpersons.txt', 'w')
+    f.write(file_superpersons)
+    a = eval(file_superpersons)
+
+
+########################## оружие
+def parsing_orusie():
+    filename = 'сиоп_оружие.csv'
+    file = open(filename, "r")
+    data = file.read()
+    rows = data.split(';";"\n')
+    # for row in rows:
+    #    print(row)
+
+    # отделяем имя персонажей и ответы на вопросы
+    split_answers = []
+    for row in rows:
+        if len(row) > 1:
+            split_answers.append([])
+            for text in row.split(';'):
+                if text:
+                    split_answers[len(split_answers) - 1].append(text.replace('\n', '\\n'))
+
+    # for a in split_answers:
+    #    print(a)
+
+    # отделим персонажей
+    persons = []
+    oruzie = []
+    answers = []
+    links = []
+    for answer in split_answers:
+        if answer:
+            persons.append([answer[0].split('/'), answer[1]])
+            oruzie.append(answer[2])
+            answers.append(answer[3:-1])
+            links.append(answer[-1])
+    # print(answers)
+    # print(persons)
+    questions = ['какое оружие у', 'что такое', 'какое оружие у']
+    file_oruzie = "    {'intents': {\n"
+    for person, answer, link, oruzi in zip(persons, answers, links, oruzie):
+        for an in answer:
+            if an:
+                file_oruzie += "        '" + questions[0] + " " + person[0][0] + "': {\n            'examples': ["
+                file_oruzie += "'" + oruzi.split(' ')[0] + "',"
+                file_oruzie += "'" + oruzi + "',"
+                for pers in person[0]:
+                    for question in questions[0:1]:
+                        file_oruzie += "'" + question + " " + pers + "',"
+                file_oruzie += "'" + questions[1] + " " + oruzi + "',"
+                file_oruzie += "],\n            'responses': ['" + an.replace('\n', '')
+                file_oruzie += ' [Фото](' + link + ')'
+                file_oruzie += "']\n        },\n"
+    file_oruzie += "    },\n}"
+    # print(file_oruzie)
+    f = open('file_oruzie.txt', 'w')
+    f.write(file_oruzie)
+    a = eval(file_oruzie)
+
+
+########################## камни
+def parsing_kamni():
+    filename = 'сиоп_камни.csv'
+    file = open(filename, "r")
+    data = file.read()
+    rows = data.split(';";"\n')
+    # for row in rows:
+    #    print(row)
+
+    # отделяем имя персонажей и ответы на вопросы
+    split_answers = []
+    for row in rows:
+        if len(row) > 1:
+            split_answers.append([])
+            for text in row.split(';'):
+                if text:
+                    split_answers[len(split_answers) - 1].append(text.replace('\n', '\\n'))
+
+    # for a in split_answers:
+    #    print(a)
+
+    # отделим персонажей
+    kvami = []
+    kamni = []
+    answers = []
+    orusie = []
+    links = []
+    for answer in split_answers:
+        if answer:
+            kvami.append(answer[0])
+            kamni.append(answer[1])
+            orusie.append(answer[2])
+            answers.append(answer[-1])
+            links.append(answer[3:5])
+    # for link in links:
+    #    print(link)
+    questions = ['какое камень у', 'какой камень у оружия', 'какое оружие у']
+    file_kamni = "    {'intents': {\n"
+    for kvam, kamen, oruz, answer, link in zip(kvami, kamni, orusie, answers, links):
+        if answer:
+            file_kamni += "        '" + questions[0] + " " + kvam + "': {\n            'examples': ["
+            file_kamni += "'" + kamen + "',"
+            file_kamni += "'" + questions[0] + " " + kvam + "',"
+            if len(oruz) < 20:
+                file_kamni += "'" + questions[1] + " " + oruz + "',"
+            file_kamni += "],\n            'responses': ['" + answer
+            file_kamni += ' [Фото](' + link[0] + ')'
+            file_kamni += "']\n        },\n"
+        if len(oruz) < 20:
+            file_kamni += "        '" + questions[2] + " " + kamen + "': {\n            'examples': ["
+            file_kamni += "'" + questions[2] + " " + kamen + "',"
+            file_kamni += "],\n            'responses': ['" + oruz
+            if link[1]:
+                file_kamni += ' [Фото](' + link[1] + ')'
+            file_kamni += "']\n        },\n"
+    file_kamni += "    },\n}"
+    # print(file_kamni)
+    f = open('file_kamni.txt', 'w')
+    f.write(file_kamni)
+    a = eval(file_kamni)
+
+
+########################## вопросы
+def parsing_voprosi():
+    filename = 'сиоп_вопросы.csv'
+    file = open(filename, "r")
+    data = file.read()
+    rows = data.split(';";"\n')
+
+    # отделяем имя персонажей и ответы на вопросы
+    split_answers = []
+    for row in rows:
+        if len(row) > 1:
+            split_answers.append([])
+            for text in row.split(';'):
+                split_answers[len(split_answers) - 1].append(text.replace('\n', '\\n'))
+
+    # отделим персонажей
+    questions = []
+    answers = []
+    links = []
+    for answer in split_answers:
+        if answer:
+            questions.append(answer[0].split('?'))
+            answers.append(answer[1].split('?'))
+            links.append(answer[2])
+
+    file_voprosi = "    {'intents': {\n"
+    for question, answer, link in zip(questions, answers, links):
+        if answer:
+            file_voprosi += "        '" + question[0] + "': {\n            'examples': ["
+            for q in question:
+                file_voprosi += "'" + q + "',"
+            file_voprosi += "],\n            'responses': ["
+            for a in answer:
+                file_voprosi += "'" + a
+                if link:
+                    file_voprosi += ' [Фото](' + link + ')'
+                file_voprosi += "',"
+            file_voprosi += "]\n        },\n"
+    file_voprosi += "    },\n}"
+    # print(file_voprosi)
+    f = open('file_voprosi.txt', 'w')
+    f.write(file_voprosi)
+    a = eval(file_voprosi)
+
+
+parsing_persons()
+parsing_superhero()
+parsing_akkym()
+parsing_kamni()
+parsing_kvami()
+parsing_orusie()
+parsing_voprosi()
---
+++ siop_1.py
@@ -0,0 +1,191 @@
+import random
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+
+BOT_CONFIG = {
+    'intents': {
+    },
+    'random_phrases': [],
+    'failure_phrases': [
+        'Попробуйте написать по другому.', 'Что-то непонятно',
+        'Возможно я не знаю.',
+    ]
+}
+
+
+def load_config(configs):
+    for intent in configs['intents'].items():
+        BOT_CONFIG['intents'][intent[0]] = {'examples': intent[1]['examples'], 'responses': intent[1]['responses']}
+
+
+def load_random(configs):
+    for intent in configs['intents'].items():
+        BOT_CONFIG['random_phrases'].append(intent[1]['responses'])
+
+
+percent = 0.1
+
+config_akkym = eval(open('file_akkym.txt', "r").read())
+config_kvami = eval(open('file_kvami.txt', "r").read())
+config_persons = eval(open('file_persons.txt', "r").read())
+config_superpersons = eval(open('file_superpersons.txt', "r").read())
+config_kamni = eval(open('file_kamni.txt', "r").read())
+config_oruzie = eval(open('file_oruzie.txt', "r").read())
+config_voprosi = eval(open('file_voprosi.txt', "r").read())
+
+load_config(config_kvami)
+load_config(config_akkym)
+load_config(config_persons)
+load_config(config_superpersons)
+load_config(config_kamni)
+load_config(config_oruzie)
+load_config(config_voprosi)
+
+load_random(config_kvami)
+load_random(config_kamni)
+load_config(config_akkym)
+load_random(config_oruzie)
+load_random(config_persons)
+load_random(config_superpersons)
+load_config(config_voprosi)
+
+
+def filter_text(text):
+    text = text.lower()
+    text = [c for c in text if c in 'абвгдеёжзийклмнопрстуфхцчшщьъыэюя- 1234567890']
+    text = ''.join(text)
+    return text
+
+
+# списки с данными и классами
+X = []
+y = []
+
+# разбиваем наш словарь на фразы и классы
+for intent, intent_data in BOT_CONFIG['intents'].items():
+    for example in intent_data['examples']:
+        X.append(filter_text(example))
+        y.append(intent)
+
+# Делаем магическую векторизацию для того что бы разделить слова
+vectorizer = CountVectorizer(ngram_range=(2, 3), analyzer="char_wb")
+X = vectorizer.fit_transform(X)
+print(vectorizer.get_feature_names())
+clf = LogisticRegression(max_iter=1000).fit(X, y)  # модель для распознования откуда вопрос
+
+
+def get_failure_phrase():
+    phrases = BOT_CONFIG['failure_phrases']
+    return random.choice(phrases)
+
+
+def generate_answer_by_text(question):
+    intent = clf.predict(vectorizer.transform([question]))
+    predict_proba = max(clf.predict_proba(vectorizer.transform([question]))[0])
+    print(' ', intent, predict_proba)
+    phrases = BOT_CONFIG['intents'][intent[0]]['responses']
+    if intent == 'расскажи что нибудь':
+        phrases = BOT_CONFIG['random_phrases']
+        phrase = random.choice(phrases)
+        return phrase[0]
+    if predict_proba > percent:
+        return random.choice(phrases)
+    else:
+        words = filter_text(question).split(' ')
+        if words:
+            answ = []
+            for intent_ in BOT_CONFIG['intents']:
+                for word in words:
+                    if word:
+                        for respons in BOT_CONFIG['intents'][intent_]['responses']:
+                            text = filter_text(respons)
+                            if word + ' ' in text:
+                                answ.append(
+                                    [intent_, BOT_CONFIG['intents'][intent_]['examples'][0],
+                                     text.count(word) / len(text.split(' '))])
+            answ.sort(key=lambda x: x[2])
+            if predict_proba > 0.03:
+                if intent != 'Привет' and intent != 'Пока':
+                    return 'К сожалению я не нахожу или не знаю ответа на данный вопрос, но возможно вы имели ' \
+                           'ввиду "' + BOT_CONFIG['intents'][intent[0]]['examples'][0] + '"? Задайте этот вопрос.'
+            else:
+                if answ:
+                    return 'К сожалению я не нахожу прямого ответа на данный вопрос, но возможно ответ есть тут: "' \
+                           + answ[len(answ) - 1][1] + '"? Задайте этот вопрос.'
+
+
+def filter_answer(answer):
+    answer = answer.replace('""', '"')
+    answer = answer.replace('??', '')
+    if answer[0] == '"':
+        answer = answer[1:]
+    answer = answer.replace('." ', '. ')
+    return answer
+
+def bot(question):
+    # Генеруем подходящий по контексту ответ
+    question = filter_text(question)
+    answer = generate_answer_by_text(question)
+    if answer:
+        answer = filter_answer(answer)
+        return answer
+    # Используем заглушку
+    answer = get_failure_phrase()
+    return answer
+
+
+print(clf.score(X, y))
+"""
+question = None
+while question not in ['exit', 'выход']:
+    question = input()
+    answer = bot(question)
+    print(answer)
+"""
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+def start(update: Update, _: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def help_command(update: Update, _: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, _: CallbackContext) -> None:
+    """Echo the user message."""
+    user = update.effective_user
+    print(user.mention_markdown_v2())
+    print('-', update.message.text)
+    answer = bot(update.message.text)
+    print(' ', answer, '\n')
+    update.message.reply_text(answer, parse_mode='markdown')
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("1825689465:AAGgkYSgZm2cK5AnonL97-GCG4kgvAyZ-6A")
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+
+main()
